d793beb7bd140bce8efa4da4b25c3ba897b7f945
dattakale21 <dattakale2008@gmail.com>
2023-07-28 23:30:54 +0530

    Initial commit

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
 
+// ------------------------------ Menu - Icon ---------------------------------------
+
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {    // onclick function for menu icons(3 lines) this is arrow function
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+}
+
 let sections = document.querySelectorAll('section');
 let navLinks = document.querySelectorAll('header nav a');
 
@@ -18,8 +28,36 @@ window.onscroll = () => {     // when the user scrolls the page on the window th
     });
 
     // ----------------------------- sticky navbar -------------------------------
-
     let header = document.querySelector('header');
-
     header.classList.toggle('sticky', window.scrollY > 100);
+
+    // ------------------------------ Remove Menu - Icon and navbar when clicked/Scroll---------------------------------------
+    menuIcon.classList.remove('bx-x');
+    navbar.classList.remove('active');
 };
+
+// ------------------------------ Scroll Reveal(Animation) ---------------------------------------
+ScrollReveal({
+    reset: true,
+    distance: '80px',
+    duration: 1800,
+    delay: 110
+});
+
+ScrollReveal().reveal('.home-content, .heading , .heading1,.home .home-image img', { origin: 'top' });
+ScrollReveal().reveal('.contact form ,.achivements-box ,.achivements-content .content h1 ,.social-media i, .btn, .about-img img', { origin: 'bottom' });
+ScrollReveal().reveal('.ed1 ,.ed2,.social-icons a,.home-content h1 , .about .about-img img', { origin: 'left' });
+
+// ------------------------------ multiple-text ---------------------------------------
+const typed = new Typed('.multiple-text', {
+    strings: ['Frontend Developer', 'Programmer', 'Explorer'],
+    typeSpeed: 80,
+    backSpeed: 80,
+    backDelay: 800,
+    loop: true
+});
+
+
+// CODE BY - DATTA KALE
+
+//  -------------------------------------- END ---------------------------------------
