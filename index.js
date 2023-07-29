01f3c33614e1303c2152e81357d9ad69f8f58dfb
dattakale21 <dattakale2008@gmail.com>
2023-07-30 00:13:38 +0530

    Initial

--- index.js
+++ index.js
@@ -44,7 +44,7 @@ ScrollReveal({
     delay: 110
 });
 
-ScrollReveal().reveal('.home-content, .heading , .heading1,.home .home-image img,.footer', { origin: 'top' });
+ScrollReveal().reveal('.home-content, .heading , .heading1,.home .home-image img', { origin: 'top' });
 ScrollReveal().reveal('.contact form ,.achivements-box ,.achivements-content .content h1 ,.social-media i, .btn, .about-img img', { origin: 'bottom' });
 ScrollReveal().reveal('.ed1 ,.ed2,.social-icons a,.home-content h1 , .about .about-img img', { origin: 'left' });
 
